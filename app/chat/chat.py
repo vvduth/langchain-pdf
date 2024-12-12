9e9fa32e389c38d1ee077d54e44a2d38cfdf6e55
vvduth <ducthai060501@gmail.com>
2024-12-12 16:21:24 +0200

    fixed some straming issue

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,4 +1,4 @@
-
+from langchain.chat_models import ChatOpenAI
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
@@ -8,6 +8,7 @@ from app.chat.memories.sql_memory import build_memory
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm=ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
