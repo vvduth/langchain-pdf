0128dedc93c320a411052c8c25372f6e0f98bef3
vvduth <ducthai060501@gmail.com>
2024-12-12 19:30:55 +0200

    add in window memory

--- app/chat/memories/window_memory.py
+++ app/chat/memories/window_memory.py
@@ -1,15 +1,12 @@
-
-from langchain.memory import ConversationBufferMemory   
+from langchain.memory import ConversationBufferWindowMemory 
 from app.chat.memories.histories.sql_history import SqlMessageHistory
 
-
-
-
-def build_memory(chat_args):
-    return ConversationBufferMemory(
+def window_buffer_memory_builder(chat_args):
+    return ConversationBufferWindowMemory(
         chat_memory=SqlMessageHistory(
             conversation_id=chat_args.conversation_id
         ),
+        k=2,
         return_messages=True,
         memory_key="chat_history",
         output_key="answer"
